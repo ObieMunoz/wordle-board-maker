de65ce9bc2e460d2c6700d4bba017468d3a2f3fb
Obie <obiemunozjr@gmail.com>
2022-02-05 16:54:27 -0500

    Update board

--- src/components/App.js
+++ src/components/App.js
@@ -1,83 +1,48 @@
 import './App.css'
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as htmlToImage from 'html-to-image';
 import { toJpeg } from 'html-to-image';
 
-function App() {
-  const [board, setBoard] = useState({
-    A1: '',
-    A2: '',
-    A3: '',
-    A4: '',
-    A5: '',
-    B1: '',
-    B2: '',
-    B3: '',
-    B4: '',
-    B5: '',
-    C1: '',
-    C2: '',
-    C3: '',
-    C4: '',
-    C5: '',
-    D1: '',
-    D2: '',
-    D3: '',
-    D4: '',
-    D5: '',
-    E1: '',
-    E2: '',
-    E3: '',
-    E4: '',
-    E5: ''
-  })
+const BOARD_OBJECT = {
+  A1: '',
+  A2: '',
+  A3: '',
+  A4: '',
+  A5: '',
+  B1: '',
+  B2: '',
+  B3: '',
+  B4: '',
+  B5: '',
+  C1: '',
+  C2: '',
+  C3: '',
+  C4: '',
+  C5: '',
+  D1: '',
+  D2: '',
+  D3: '',
+  D4: '',
+  D5: '',
+  E1: '',
+  E2: '',
+  E3: '',
+  E4: '',
+  E5: '',
+  F1: '',
+  F2: '',
+  F3: '',
+  F4: '',
+  F5: ''
+}
 
-  function handleChange(e) {
-    if (e.target.value.length > 1) {
-      return
-    } else {
-      setBoard({
-        ...board,
-        [e.target.name]: e.target.value.toUpperCase()
-      })
-    }
-    if (e.target.value === '') {
-      e.target.previousElementSibling.focus()
-    } else {
-      e.target.nextElementSibling.focus()
-    }
-    console.log(board)
-    console.log(e.target.name);
-  }
+function App() {
+  const [board, setBoard] = useState(BOARD_OBJECT)
+  const [input, setInput] = useState('')
 
   function handleReset() {
-    setBoard({
-      A1: '',
-      A2: '',
-      A3: '',
-      A4: '',
-      A5: '',
-      B1: '',
-      B2: '',
-      B3: '',
-      B4: '',
-      B5: '',
-      C1: '',
-      C2: '',
-      C3: '',
-      C4: '',
-      C5: '',
-      D1: '',
-      D2: '',
-      D3: '',
-      D4: '',
-      D5: '',
-      E1: '',
-      E2: '',
-      E3: '',
-      E4: '',
-      E5: ''
-    })
+    setBoard(BOARD_OBJECT)
+    setInput('')
   }
 
   function handleColor(e) {
@@ -106,50 +71,102 @@ function App() {
       });
   }
 
+  function handleUpdateBoard(e) {
+    const letters = e.target.value.toUpperCase().split('')
+    setBoard({
+      A1: letters[0] || '',
+      A2: letters[1] || '',
+      A3: letters[2] || '',
+      A4: letters[3] || '',
+      A5: letters[4] || '',
+      B1: letters[5] || '',
+      B2: letters[6] || '',
+      B3: letters[7] || '',
+      B4: letters[8] || '',
+      B5: letters[9] || '',
+      C1: letters[10] || '',
+      C2: letters[11] || '',
+      C3: letters[12] || '',
+      C4: letters[13] || '',
+      C5: letters[14] || '',
+      D1: letters[15] || '',
+      D2: letters[16] || '',
+      D3: letters[17] || '',
+      D4: letters[18] || '',
+      D5: letters[19] || '',
+      E1: letters[20] || '',
+      E2: letters[21] || '',
+      E3: letters[22] || '',
+      E4: letters[23] || '',
+      E5: letters[24] || '',
+      F1: letters[25] || '',
+      F2: letters[26] || '',
+      F3: letters[27] || '',
+      F4: letters[28] || '',
+      F5: letters[29] || ''
+    })
+    if (e.target.value.length < 31) {
+      setInput(e.target.value)
+    }
+  }
+
   return (
     <>
       <div id="intro" style={{ color: 'white' }}>
         <h1>Wordle Board Maker</h1>
         <h3>Instructions:</h3>
-        <p>1. Enter the letters you want on the board</p>
-        <p>2. Click each cell to change their colors</p>
-        <p>3. Press save to save your board image!</p>
+        <span>1. Enter the letters you want on the board</span><br></br>
+        <span>2. Click each cell to change their colors</span><br></br>
+        <span>3. Press save to save your board image!</span><br></br>
+        <br />
       </div>
+
       <div id="board">
-        <input type="text" id="word" name="A1" onClick={handleColor} value={board.A1} onChange={handleChange} />
-        <input type="text" id="word" name="A2" onClick={handleColor} value={board.A2} onChange={handleChange} />
-        <input type="text" id="word" name="A3" onClick={handleColor} value={board.A3} onChange={handleChange} />
-        <input type="text" id="word" name="A4" onClick={handleColor} value={board.A4} onChange={handleChange} />
-        <input type="text" id="word" name="A5" onClick={handleColor} value={board.A5} onChange={handleChange} />
+        <p name="A1" onClick={handleColor}>{board.A1}</p>
+        <p name="A2" onClick={handleColor}>{board.A2}</p>
+        <p name="A3" onClick={handleColor}>{board.A3}</p>
+        <p name="A4" onClick={handleColor}>{board.A4}</p>
+        <p name="A5" onClick={handleColor}>{board.A5}</p>
         <br />
-        <input type="text" id="word" name="B1" onClick={handleColor} value={board.B1} onChange={handleChange} />
-        <input type="text" id="word" name="B2" onClick={handleColor} value={board.B2} onChange={handleChange} />
-        <input type="text" id="word" name="B3" onClick={handleColor} value={board.B3} onChange={handleChange} />
-        <input type="text" id="word" name="B4" onClick={handleColor} value={board.B4} onChange={handleChange} />
-        <input type="text" id="word" name="B5" onClick={handleColor} value={board.B5} onChange={handleChange} />
+        <p name="B1" onClick={handleColor}>{board.B1}</p>
+        <p name="B2" onClick={handleColor}>{board.B2}</p>
+        <p name="B3" onClick={handleColor}>{board.B3}</p>
+        <p name="B4" onClick={handleColor}>{board.B4}</p>
+        <p name="B5" onClick={handleColor}>{board.B5}</p>
         <br />
-        <input type="text" id="word" name="C1" onClick={handleColor} value={board.C1} onChange={handleChange} />
-        <input type="text" id="word" name="C2" onClick={handleColor} value={board.C2} onChange={handleChange} />
-        <input type="text" id="word" name="C3" onClick={handleColor} value={board.C3} onChange={handleChange} />
-        <input type="text" id="word" name="C4" onClick={handleColor} value={board.C4} onChange={handleChange} />
-        <input type="text" id="word" name="C5" onClick={handleColor} value={board.C5} onChange={handleChange} />
+        <p name="C1" onClick={handleColor}>{board.C1}</p>
+        <p name="C2" onClick={handleColor}>{board.C2}</p>
+        <p name="C3" onClick={handleColor}>{board.C3}</p>
+        <p name="C4" onClick={handleColor}>{board.C4}</p>
+        <p name="C5" onClick={handleColor}>{board.C5}</p>
         <br />
-        <input type="text" id="word" name="D1" onClick={handleColor} value={board.D1} onChange={handleChange} />
-        <input type="text" id="word" name="D2" onClick={handleColor} value={board.D2} onChange={handleChange} />
-        <input type="text" id="word" name="D3" onClick={handleColor} value={board.D3} onChange={handleChange} />
-        <input type="text" id="word" name="D4" onClick={handleColor} value={board.D4} onChange={handleChange} />
-        <input type="text" id="word" name="D5" onClick={handleColor} value={board.D5} onChange={handleChange} />
+        <p name="D1" onClick={handleColor}>{board.D1}</p>
+        <p name="D2" onClick={handleColor}>{board.D2}</p>
+        <p name="D3" onClick={handleColor}>{board.D3}</p>
+        <p name="D4" onClick={handleColor}>{board.D4}</p>
+        <p name="D5" onClick={handleColor}>{board.D5}</p>
         <br />
-        <input type="text" id="word" name="E1" onClick={handleColor} value={board.E1} onChange={handleChange} />
-        <input type="text" id="word" name="E2" onClick={handleColor} value={board.E2} onChange={handleChange} />
-        <input type="text" id="word" name="E3" onClick={handleColor} value={board.E3} onChange={handleChange} />
-        <input type="text" id="word" name="E4" onClick={handleColor} value={board.E4} onChange={handleChange} />
-        <input type="text" id="word" name="E5" onClick={handleColor} value={board.E5} onChange={handleChange} />
+        <p name="E1" onClick={handleColor}>{board.E1}</p>
+        <p name="E2" onClick={handleColor}>{board.E2}</p>
+        <p name="E3" onClick={handleColor}>{board.E3}</p>
+        <p name="E4" onClick={handleColor}>{board.E4}</p>
+        <p name="E5" onClick={handleColor}>{board.E5}</p>
+        <br />
+        <p name="F1" onClick={handleColor}>{board.F1}</p>
+        <p name="F2" onClick={handleColor}>{board.F2}</p>
+        <p name="F3" onClick={handleColor}>{board.F3}</p>
+        <p name="F4" onClick={handleColor}>{board.F4}</p>
+        <p name="F5" onClick={handleColor}>{board.F5}</p>
       </div>
       <br></br>
       <br></br>
+      <input type="text" value={input} onChange={handleUpdateBoard} style={{ width: '350px' }} />
+      <br />
       <button onClick={handleSave}>Save</button>
       <button onClick={handleReset}>Reset</button>
+
+      <br />
+      <br />
     </>
   );
 }
