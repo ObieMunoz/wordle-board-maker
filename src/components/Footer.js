05ee99a17d95f4b7544f8537ec048f1199daeda6
Obie <obiemunozjr@gmail.com>
2022-02-05 23:37:58 -0500

    Update verbiage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,6 @@ export default function Footer() {
     return <div id="footer">
         Made by <a href="https://www.linkedin.com/in/obedmunozjr/">Obie Munoz</a><br />
         Explore this project on <a href="https://www.github.com/obiemunoz/wordle-board-maker">Github</a><br />
-        Problems?Submit an issue on <a href="https://github.com/ObieMunoz/wordle-board-maker/issues/new">Github</a><br />
+        Problems? Submit a <a href="https://github.com/ObieMunoz/wordle-board-maker/issues/new">new issue</a><br />
     </div>;
 }
